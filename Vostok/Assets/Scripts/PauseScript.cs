6a9e6b91e3332c995fe5db7d75906c5b648987c4
Igor <pacu2901@gmail.com>
2018-06-20 19:24:11 -0300

    Contrução do pause Script

---
+++ Vostok/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseScript : MonoBehaviour {
+
+	public bool paused;
+
+	// Use this for initialization
+	void Start () {
+		paused = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	public void Pause(){
+
+		Debug.Log ("Entrou no pause");
+		paused = !paused;
+
+		if (paused) {
+			Time.timeScale = 0;
+		} else if (!paused) {
+			Time.timeScale = 1;
+		}
+
+
+
+	}	
+}
