c102fe027ac2211b7dc49f028b36ef4d67e5de93
LAPTOP-QOMAK5JF\CtrlPlat Aluno 03 <reis10leticia@gmail.com>
2018-06-20 19:25:36 -0300

    Adicionando CollsionController e Prefab(DeathCollider)

---
+++ Vostok/Assets/Scripts/CollisionControler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionControler : MonoBehaviour {
+
+	void OnCollisionEnter (Collision col)  //função da colisão
+	{
+		if (col.gameObject.name == "Person") 
+		{
+			Destroy (col.gameObject);
+		}
+		
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
