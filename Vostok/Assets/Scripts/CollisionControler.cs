fa40b469806212182bd56aa81f93c92969123135
LAPTOP-QOMAK5JF\CtrlPlat Aluno 03 <reis10leticia@gmail.com>
2018-07-11 19:16:54 -0300

    Person retorna a posição inicial quando colide com algum obstáculo por meio da posição do GameObject Marker1_start

--- Vostok/Assets/Scripts/CollisionControler.cs
+++ Vostok/Assets/Scripts/CollisionControler.cs
@@ -3,22 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CollisionControler : MonoBehaviour {
-	
-	void OnCollisionEnter (Collision col) {
-		if (col.gameObject.name == "Person") {
-			Destroy (col.gameObject);
-		}
-	}
+
 
 	// Use this for initialization
 	void Start () {
-		
-		
 	}
-	
+
+
+	void OnCollisionEnter (Collision col)  //função da colisão
+	{
+		if (col.gameObject.name == "Person") 
+		{ 
+			col.gameObject.transform.position = GameObject.Find("Marker1_start").transform.position;
+			col.gameObject.GetComponent<Rigidbody>().AddForce (Vector3.forward * gameObject.GetComponent<MovementController> ().f_force * (-1));
+		}
+	}
+
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
-
