44ee5d00492c20d25b4c70059932617ec71709cd
LAPTOP-QOMAK5JF\CtrlPlat Aluno 03 <reis10leticia@gmail.com>
2018-08-08 19:14:23 -0300

    DeathCollider

---
+++ Vostok/Assets/Scripts/DeathCollider.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathCollider : MonoBehaviour {
+
+	public string currentPlaceHolder;
+
+	void OnCollisionEnter (Collision death)  {
+		if (death.gameObject.name == "Person") { 
+			restartLevel (1);
+		}
+	}
+
+	void Start () {
+
+	}
+
+
+	void Update () {
+
+	}
+
+	public void restartLevel (int level) {
+		if (level == 1) {
+			GameObject.Find("Person").transform.position = GameObject.Find(currentPlaceHolder).transform.position;
+		}
+
+	}
+
+}
