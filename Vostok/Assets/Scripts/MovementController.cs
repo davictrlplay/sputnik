a071cb4ca7281da3029eb3685869c7442d7fd0a1
Koritar <jakoritarl@gmail.com>
2018-09-12 19:38:23 -0300

    Movement Controller com Swipe

--- Vostok/Assets/Scripts/MovementController.cs
+++ Vostok/Assets/Scripts/MovementController.cs
@@ -1,8 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MovementController : MonoBehaviour {
+	
+	public bool tap, swipeLeft, swipeRight, swipeUp, swipeDown;
+	public bool isDraging;
+	public Vector2 startTouch, swipeDelta;
+
 
 	public float f_force; // Variavel criada para força
 	public int i_forceLevel; // Variavel criada para contar o nivel da força
@@ -14,6 +20,8 @@ public class MovementController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		isDraging = false;
+
 		rb_person = this.GetComponent<Rigidbody> ();
 		f_force = 30f; // Adiciona o valor de 5 à variavel f_force
 		f_sides = 0.5f; // Adiciona o valor de 4 à variavel f_sides
@@ -23,6 +31,62 @@ public class MovementController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+
+		tap = swipeLeft = swipeRight = swipeUp = swipeDown = false;
+
+		#region Standalone Inputs
+		if (Input.GetMouseButtonDown(0)) {
+			tap = true;
+			isDraging = true;
+			startTouch = Input.mousePosition;
+		} else if (Input.GetMouseButtonUp(0)) {
+			isDraging = false;
+			Reset();
+		}
+		#endregion
+
+		//////
+		#region Mobile Inputs
+		if (Input.touches.Length > 0) {
+			if (Input.touches[0].phase == TouchPhase.Began) {
+				tap = true;
+				isDraging = true;
+				startTouch = Input.touches[0].position;
+			} else if (Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled) {
+				isDraging = false;
+				Reset();
+			}
+		}
+		#endregion
+
+		swipeDelta = Vector3.zero;
+		if (isDraging) {
+			if (Input.touches.Length > 0) {
+				swipeDelta = Input.touches [0].position - startTouch;
+			} else if (Input.GetMouseButton (0)) {
+				swipeDelta = (Vector2)Input.mousePosition - startTouch;
+			}
+		}
+
+		if (swipeDelta.magnitude > 125) {
+			float x = swipeDelta.x;
+			float y = swipeDelta.y;
+
+			if (Mathf.Abs (x) > Mathf.Abs (y)) {
+				if (x < 0)
+					swipeLeft = true;
+				else
+					swipeRight = true;
+			} else {
+				if (y < 0)
+					swipeDown = true;
+				else
+					swipeUp = true;
+
+			}
+
+			Reset ();
+		}
 
 		//Para aumentar a velocidade do objeto
 		if (Input.GetKeyDown (KeyCode.W)) { //Se a tecla pressionada for W
@@ -47,7 +111,7 @@ public class MovementController : MonoBehaviour {
 		}
 
 		//Para ir para direita
-		if (Input.GetKeyDown (KeyCode.D)) { //Se a tecla pressionada for D
+		if (swipeRight) { //Se a tecla pressionada for D
 			this.transform.Translate (f_sides, 0f, 0f); //Adiciona o valor de f_sides para o eixo.X usando o transform
 		}
 
@@ -61,6 +125,18 @@ public class MovementController : MonoBehaviour {
 		}
 
 	}
+
+	public void Reset() {
+		startTouch = swipeDelta = Vector3.zero;
+		isDraging = false;
+	}
+
+	public Vector3 SwipeDelta { get { return swipeDelta; } }
+	public bool SwipeLeft { get { return swipeLeft; } }
+	public bool SwipeRight { get { return swipeRight; } }
+	public bool SwipeUp { get { return swipeUp; } }
+	public bool SwipeDown { get { return swipeDown; } }
+
 }
 
 
