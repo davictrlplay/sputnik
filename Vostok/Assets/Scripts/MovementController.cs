6cbe6066cd5b0e10086bba72e19777875b0de706
LAPTOP-312G1F75\Ctrl Play Aluno 12 <l.alvesctrlplay@gmail.com>
2018-10-03 18:43:19 -0300

    Criação da fase 4 e correção da fase 3.

--- Vostok/Assets/Scripts/MovementController.cs
+++ Vostok/Assets/Scripts/MovementController.cs
@@ -89,7 +89,7 @@ public class MovementController : MonoBehaviour {
 		}
 
 		//Para aumentar a velocidade do objeto
-		if (swipeDown) { //Se a tecla pressionada for W
+		if (swipeDown || Input.GetKeyDown (KeyCode.W)) { //Se a tecla pressionada for W
 			if (i_forceLevel <= 10) { //Se o nivel da força for menor/igual a 10
 				rb_person.AddForce (Vector3.forward * f_force); //Adiciona força para frente no objeto
 				i_forceLevel++; //Adiciona o valor de 1 para o nivel da força
@@ -100,7 +100,7 @@ public class MovementController : MonoBehaviour {
 		}
 
 		//Para diminuir a velocidade do objeto
-		if (Input.GetKey (KeyCode.S)) { //Se a tecla pressionada for S
+		if (Input.GetKeyDown (KeyCode.S)) { //Se a tecla pressionada for S
 			if (i_forceLevel >= 1) { //Se o nivel da força for maior/igual a 1
 				rb_person.AddForce (Vector3.back * f_force); //Adiciona força para trás no objeto
 				i_forceLevel--; //Subtrai o valor de 1 para o nivel da força
@@ -111,21 +111,22 @@ public class MovementController : MonoBehaviour {
 		}
 
 		//Para ir para direita
-		if (swipeRight) { //Se a tecla pressionada for D
+		if (swipeRight || Input.GetKeyDown (KeyCode.D)) { //Se a tecla pressionada for D
 			this.transform.Translate (f_sides, 0f, 0f); //Adiciona o valor de f_sides para o eixo.X usando o transform
 		}
 
 		//Para ir para esquerda
-		if (swipeLeft) { //Se a tecla pressionada for A
+		if (swipeLeft || Input.GetKeyDown (KeyCode.A)) { //Se a tecla pressionada for A
 			this.transform.Translate (-f_sides, 0f, 0f); //Remove o valor de f_sides para o eixo.X usando o transform
 		}
 
-		if (swipeUp && this.transform.position.y <= 0.8) { //Se a tecla pressionada for Space
+		if (swipeUp && this.transform.position.y <= 0.8 || Input.GetKeyDown (KeyCode.Space) && this.transform.position.y <= 0.8) { //Se a tecla pressionada for Space
 			Debug.Log ("Pulo");
 			rb_person.AddForce (Vector3.up * i_forceUp); //Adiciona força de i_forceUp
 		}
 
 	}
+
 
 	public void Reset() {
 		startTouch = swipeDelta = Vector3.zero;
